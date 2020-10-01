d01a732b809378e47509b41852c5ed59e4c95e14
sukaran <32487359+sukaran@users.noreply.github.com>
2020-10-02 00:03:13 +0530

    Add files via upload

---
+++ binary_tree.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+	int data;
+	node* left,*right;
+	public:
+		node(int data){
+			this->data;
+			this->left=this->right = NULL;
+		}
+};
+int main(){
+	node* head;
+	while(1){
+		int n;
+		cin>>n;
+		
+		if(n==-1){
+			break;
+		}
+		node tail = new node(n);
+		
+	}
+	
+}
